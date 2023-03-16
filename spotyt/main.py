16ae39f3fb74b1f2f46c1ead50f053320c3f7ab9
Daniel Vasquez <nielvas.dev@gmail.com>
2023-03-16 13:34:40 -0700

    fix(script): react production cdn and crossorigin

--- spotyt/main.py
+++ spotyt/main.py
@@ -30,10 +30,6 @@ app.mount(
 
 templates = Jinja2Templates(directory="spotyt/templates")
 
-
-
-ptracks = [{"id":"6k8oac03vALHfrb9tPuOc7","artist":"Silvia Tarozzi Deborah Walker","name":"Pietà l‘è morta"},{"id":"5gJw9DpcnYywIIVGYSb4Y5","artist":"Weyes Blood","name":"God Turn Me Into a Flower"},{"id":"3s3vybdkzM8sHztxIEQcYr","artist":"Mount Eerie","name":"Moon, I Already Know"},{"id":"1yErlfGE8brAL5ZTo7maBU","artist":"Mooreiy","name":"Beloved"},{"id":"3bgH7ay0vCi8pqaFq4H9UY","artist":"Early Fern Joseph Shabason","name":"Softly Brushed By Wind"},{"id":"7uECCWx2tz4T102b2QBS0p","artist":"Yves Tumor","name":"Echolalia"},{"id":"5MEfxTan4X3PmkETXxOG6e","artist":"Maraschino","name":"Angelface"},{"id":"1lUT9ZnrWDVbLdnPyzjYIq","artist":"Fox the Fox","name":"Flirting And Showing"},{"id":"79G1B6sFPHiA1xh4OGWtOz","artist":"Robert Sandrini","name":"Occhi Su Di Me"},{"id":"1Ixa4ZLNWbaMsb6vLnzBKV","artist":"Scribble","name":"Mother of Pearl"},{"id":"7J46lkQn2onLYyg4PtJEJk","artist":"Marlene Ribeiro","name":"You Do It"},{"id":"3mbdc8LQR0tCPdV0d3sruO","artist":"Pablo's Eye","name":"La Pedrera"},{"id":"4dL0IByCuNLmydwMRFokIM","artist":"Discovery Zone","name":"Fall Apart"},{"id":"7hFfYDQIaMlwfMqi6mOCtv","artist":"Clive Stevens & Brainchild","name":"Mystery Man"},{"id":"2gDXrWHc7ovpvAauol3PjZ","artist":"International Music System","name":"Vanity Rap - 116 Bpm"},{"id":"1ihoAOte7bTCUbvWxBR3fk","artist":"The Durutti Column","name":"Spasmic Fairy"}]
-
 @app.get("/api/playlist/{playlist_id}/")
 def get_playlist_tracks(
     playlist_id: str,
